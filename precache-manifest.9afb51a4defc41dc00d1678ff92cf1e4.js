66abea2f3598d99eba6c18517192245f399a0ce2
Sebastian <sebasmarro10@gmail.com>
2020-08-13 16:53:22 -0500

    Updates

---
+++ precache-manifest.9afb51a4defc41dc00d1678ff92cf1e4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c69e701dd753f3e2ae7a40dfe098e5",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "f1165ec4d172a53d9546",
+    "url": "/corona-tracker/static/css/main.43fbe9ae.chunk.css"
+  },
+  {
+    "revision": "09971153d707d9a8ea0a",
+    "url": "/corona-tracker/static/js/2.89ba18e4.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.89ba18e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1165ec4d172a53d9546",
+    "url": "/corona-tracker/static/js/main.d07dac3d.chunk.js"
+  },
+  {
+    "revision": "153bc3447451881c9dc1",
+    "url": "/corona-tracker/static/js/runtime-main.567df195.js"
+  },
+  {
+    "revision": "86a2f13e91ac85080ebaeaab9463b9f1",
+    "url": "/corona-tracker/static/media/Poppins-ExtraLight.86a2f13e.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/corona-tracker/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "20e9836494a4f7af166a5cc146906d5e",
+    "url": "/corona-tracker/static/media/ico.20e98364.png"
+  }
+]);
